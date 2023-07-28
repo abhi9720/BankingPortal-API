a9f1e6973161a22d3b9ac4f462535d7e4a6010f3
abhishektiwari02 <abhishek.tiwari02@nagarro.com>
2023-07-29 01:20:36 +0530

    otp async functionality added

--- src/main/java/com/webapp/bankingportal/service/EmailService.java
+++ src/main/java/com/webapp/bankingportal/service/EmailService.java
@@ -1,8 +1,8 @@
 package com.webapp.bankingportal.service;
 
-
+import java.util.concurrent.CompletableFuture;
 
 public interface EmailService {
-	public void sendEmail(String to, String subject, String text);
+	public CompletableFuture<Void> sendEmail(String to, String subject, String text);
     public String getOtpLoginEmailTemplate(String name,String accountNumber, String otp) ;
 }
