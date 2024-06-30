2fce6c52e8205eba6c12e6282b602d252d54ddb9
abhi9720 <abhishektiwari9720@gmail.com>
2024-06-30 23:01:58 +0530

    swagger documentation added with jwt token

--- src/main/java/com/webapp/bankingportal/config/SwaggerConfig.java
+++ src/main/java/com/webapp/bankingportal/config/SwaggerConfig.java
@@ -1,6 +1,39 @@
 package com.webapp.bankingportal.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
-   
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "Bearer";
+        return new OpenAPI()
+                .info(new Info().title("Banking Portal API")
+                        .description("This is auth service use for validate the user.")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("BankingPortal-API Wiki Documentation")
+                        .url("https://github.com/abhi9720/BankingPortal-API/wiki"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
 }
